2d44fdf16671d23300b2c7c75180ac4eeb259a2b
erisalke <erisalke@gmail.com>
2016-02-11 21:27:58 +0100

    Add hash and print good

--- Program.cs
+++ Program.cs
@@ -1,45 +1,109 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace morseCodeDecypher
 {
     public class Program
     {
+        private static IDictionary GetLetterCodes()
+        {
+            return new Dictionary<string,string>()
+                    {
+                      { ".-", "a" },
+                      { "-...", "b" },
+                      { "-.-.", "c" },
+                      // "-.."  : "d",
+                      // "."    : "e",
+                      // "..-." : "f",
+                      // "--."  : "g",
+                      // "...." : "h",
+                      // ".."   : "i",
+                      // ".---" : "j",
+                      // "-.-"  : "k",
+                      // ".-.." : "l",
+                      // "--"   : "m",
+                      // "-."   : "n",
+                      // "---"  : "o",
+                      // ".--." : "p",
+                      // "--.-" : "q",
+                      // ".-."  : "r",
+                      // "..."  : "s",
+                      // "-"    : "t",
+                      // "..-"  : "u",
+                      // "...-" : "v",
+                      // ".--"  : "w",
+                      // "-..-" : "x",
+                      // "-.--" : "y",
+                      // "--.." : "z"
+                    };
+        }
+
+        private static IEnumerable ReadDictionaryFromFile(){
+          return new List<string>() { "word1", "word2" };
+        }
+
+        private static string GetEncryptedMessage(){
+            return "-.-";
+        }
+
+        private static void JustWriteToConsole(IEnumerable sentences){
+          foreach(string x in sentences){
+            Console.WriteLine(x);
+          }
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine();
             Console.WriteLine("--------- Morse Code Decypher v.1.0 ---------");
 
-            ReadDataFromFile();
-            DoJob();
+            var message = GetEncryptedMessage();
+            var letterCodesHash = GetLetterCodes();
+            var dictionary = ReadDictionaryFromFile();
 
+            var decoder = new Decoder( message,
+                                       letterCodesHash,
+                                       dictionary);
+
+            decoder.Calculate();
+
+            JustWriteToConsole(decoder.ReturnResults());
             //Console.Read();
-        }
-
-        private static
-        public static void DoJob(){
-
         }
     }
 
     public class Decoder
     {
         private string factoryValue;
+        private IDictionary letterCodesHash;
+        private IEnumerable dictionary;
+        private IEnumerable results;
 
         public Decoder(string encodedMsg,
-                       IDictionary<string, string> letterCodesHash,
-                       IEnumebable<string> dictionary)
+                       IDictionary letterCodesHash,
+                       IEnumerable dictionary)
         {
             this.factoryValue = encodedMsg;
             this.letterCodesHash = letterCodesHash;
             this.dictionary = dictionary;
+
+            this.results = new List<string>(){
+                              "not yet ready man",
+                              "but getting better"
+                            };
         }
 
-        public void Calculate(){
-            
+        public void Calculate()
+        {
+// damnstillempty... :)
         }
 
-        public string ReturnResult(){
-          return "not yet ready man";
+        // public IList ReturnResults()
+        public IEnumerable<string> ReturnResults()
+        {
+            return (this.results as IEnumerable<string>);
         }
     }
 }
