79e1a14b968ec4093d5506b5ade7bc5d66e4c8d4
erisalke <erisalke@gmail.com>
2016-02-11 19:31:14 +0100

    Skeleton

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace morseCodeDecypher
 {
@@ -9,8 +6,40 @@ namespace morseCodeDecypher
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.Read();
+            Console.WriteLine();
+            Console.WriteLine("--------- Morse Code Decypher v.1.0 ---------");
+
+            ReadDataFromFile();
+            DoJob();
+
+            //Console.Read();
+        }
+
+        private static
+        public static void DoJob(){
+
+        }
+    }
+
+    public class Decoder
+    {
+        private string factoryValue;
+
+        public Decoder(string encodedMsg,
+                       IDictionary<string, string> letterCodesHash,
+                       IEnumebable<string> dictionary)
+        {
+            this.factoryValue = encodedMsg;
+            this.letterCodesHash = letterCodesHash;
+            this.dictionary = dictionary;
+        }
+
+        public void Calculate(){
+            
+        }
+
+        public string ReturnResult(){
+          return "not yet ready man";
         }
     }
 }
