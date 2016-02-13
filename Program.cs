f7ba7df1e21029e2f737446ca53447192915b299
erisalke <erisalke@gmail.com>
2016-02-13 21:01:48 +0100

    WORKS!

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace morseCodeDecypher
 {
     public class Program
     {
-        private static IDictionary GetLetterCodes()
+        private static Dictionary<string,string> GetLetterCodes()
         {
             return new Dictionary<string,string>()
                     {
@@ -48,8 +48,8 @@ namespace morseCodeDecypher
         }
 
         private static string GetEncryptedMessage(){
-            //var str = ".--..-..-.-.-----.-----....--...-.-.-..-....--.-......----."; // orgiginal?
-            var str = ".--..-..-.-.-----.-----....--...-.-.-..-....--.-......----."; // "SOS HELP" & ?
+            var str = ".--..-..-.-.-----.-----....--...-.-.-..-....--.-......----.";
+                      //  .--..-..-.-.-----.-----....--...-.-.-..-....--.-......----.
             return str;
         }
 
@@ -68,14 +68,43 @@ namespace morseCodeDecypher
             var letterCodesHash = GetLetterCodes() as Dictionary<string,string>;
             var dictionary = ReadDictionaryFromFile();
 
-            var decoder = new Decoder( message,
-                                       letterCodesHash,
-                                       dictionary);
-
+            var decoder = new Decoder(message, letterCodesHash, dictionary);
             decoder.Calculate();
             JustWriteToConsole(decoder.ReturnResults());
-            //Console.Read();
+
+            var encoder = new Encoder((List<string>)decoder.ReturnResults(), letterCodesHash);
+            encoder.Encode();
+            JustWriteToConsole(encoder.ReturnResults());
         }
+    }
+
+    public class Encoder
+    {
+      private List<string> results;
+      private List<string> messages;
+      private Dictionary<string,string> letterCodesHash;
+
+      public Encoder(List<string> messages,Dictionary<string,string> letterCodesHash){
+        this.messages = messages;
+        this.letterCodesHash = letterCodesHash;
+        this.results = new List<string>();
+      }
+
+      public void Encode(){
+        foreach(var m in this.messages){
+          var result ="";
+          m.Replace(" ", "");
+          for (var i=0; i<m.Length; ++i){
+            result += this.letterCodesHash.FirstOrDefault(x => x.Value == m[i].ToString()).Key;
+          }
+          this.results.Add(result);
+        }
+      }
+
+      public IEnumerable<string> ReturnResults()
+      {
+          return (this.results as IEnumerable<string>);
+      }
     }
 
     public class Decoder
@@ -86,57 +115,50 @@ namespace morseCodeDecypher
         private List<string> results;
 
         public Decoder(string encodedMsg,
-                       IDictionary letterCodesHash,
+                       Dictionary<string,string> letterCodesHash,
                        List<string> dictionary)
         {
             this.factoryValue = encodedMsg;
-            this.letterCodesHash = (letterCodesHash as Dictionary<string,string>);
+            this.letterCodesHash = letterCodesHash;
             this.dictionary = dictionary;
-
-            this.results = new List<string>(){
-                              "not yet ready man",
-                              "but getting better"
-                            };
+            this.results = new List<string>();
         }
-
-        // private IEnumerable GetPossibilities(string word, ){
-        //   var newChar =
-        //   return this.dictionary.Where(x => x.containsOrStartsWith(word+newChar));
-        // }
 
         public void Calculate(int head=0, string word="", string sentence="")
         {
-          if (head == this.factoryValue.Length -1){
-            //all makes sense check?
-              // Add solution to List of solutions
-              this.results.Add(sentence);
-          }
-          else{
+          if (head <= this.factoryValue.Length){
             string value = "";
-            for (var offset = 1; offset<Math.Min(5, this.factoryValue.Length-head); offset++){
-              string ch = this.factoryValue.Substring(head, offset);
+            var gggg = Math.Min(4, this.factoryValue.Length-head);
+
+
+if (head+1 == this.factoryValue.Length){
+  Console.WriteLine("H{0}, {1}, {2}", head, this.factoryValue.Length, this.factoryValue.Substring(head, 1));
+}
+            for (var offset = 1; offset<=gggg; offset++){
+              var ch = this.factoryValue.Substring(head, offset);
               if (this.letterCodesHash.TryGetValue(ch, out value))
               {
                 var possibleWords = this.dictionary.Where(x=>x.StartsWith(word+value));
                 if (possibleWords.Count() > 0){
                   var dup = possibleWords.FirstOrDefault(x=>x == word+value);
                   if(dup != null){
-                    Console.WriteLine("Head {0}, word: {1}", head, word+value);
+                    Console.WriteLine("{0}, {1}",head+offset,this.factoryValue.Length);
+                    if (head+offset == this.factoryValue.Length){
+                      this.results.Add(sentence+word+value);
+                    Console.WriteLine("Head {0}, word: {1}", head, sentence+word+value+" "+"***");
+                    }
+
+                    Console.WriteLine("Head {0}, word: {1}", head, sentence+word+value+" ");
                     Calculate(head+offset, "", sentence+word+value+" ");
                   }
 
                   Calculate(head+offset, word+value, sentence);
                 }
               }
-
-              // Calculate(head+offset, word+"a", sentence+word);
             }
           }
         }
 
-
-
-        // public IList ReturnResults()
         public IEnumerable<string> ReturnResults()
         {
             return (this.results as IEnumerable<string>);
